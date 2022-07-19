3fb8d348aebb7aa1d562006c2e39a037bfc9f7a7
Vinícius Alves <viniciusalvessgp@hotmail.com>
2022-07-18 23:37:02 -0300

    style: create screens layout

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/styles/theme';
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading /> }
+      {fontsLoaded ? <Routes /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
